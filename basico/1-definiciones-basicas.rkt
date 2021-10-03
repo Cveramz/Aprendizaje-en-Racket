68ca167e61f913fd484e6053d14a31b89d8d6df4
CarlosVeRamz <83312809+Cveramz@users.noreply.github.com>
2021-10-03 00:51:34 -0300

    Create 1-definiciones-basicas.rkt

---
+++ basico/1-definiciones-basicas.rkt
@@ -0,0 +1,14 @@
+#lang racket
+; Al definir caracteres se agregan comillas
+(define nombre "eduardo")
+;Al definir un numero se hace así
+(define edad 20)
+#|
+Todo esto es un comentario de muchas lineas
+:D
+|#
+
+;f()Media poner dos numeros y dividirlos en dos.
+
+(define (media a b) (/(+ a b) 2))
+;en (media a b) son los argumentos - lo de la derecha es la salida
